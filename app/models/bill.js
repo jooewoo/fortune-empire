612e398159b502964558eb2d2d0ed520f21776f8
jooewoo <jooewoo@gmail.com>
2019-01-18 14:40:21 -0500

    Adds Model, Routes, and curl scripts
    Adds Bill model with 5 different parameters.
    Adds Bill route with index, show, create, destroy, and update routes
    Adds Bill model and route to the server.js
    Adds Bill curl scripts and tested that all curl scripts are working.

---
+++ app/models/bill.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const billSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Bill', billSchema)
